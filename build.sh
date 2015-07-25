ca71687d2dcf5db7a2fac2712059de48840507e9
Hajime Hoshi <hajimehoshi@gmail.com>
2015-07-26 00:16:44 +0900

    Modify createdefualtsripts.go to accept multiple files

--- build.sh
+++ build.sh
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-go run createdefaultscripts.go < editor/defaultscript.js > editor/defaultscripts.ts
+go run createdefaultscripts.go editor/defaultscripts/*.js > editor/defaultscripts.ts
 tsc --out editor.js --target ES5 data/*.ts editor/*.ts
 tsc --out player.js --target ES5 data/*.ts player/*.ts
