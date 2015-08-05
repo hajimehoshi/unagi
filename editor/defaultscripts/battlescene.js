e9e2b44b861e0878e135ebf4a55694c1686534ec
Hajime Hoshi <hajimehoshi@gmail.com>
2015-08-05 11:59:20 +0900

    Show actors in the battle scene

--- editor/defaultscripts/battlescene.js
+++ editor/defaultscripts/battlescene.js
@@ -1,10 +1,22 @@
 'use strict'
 
+function imageById(id) {
+    if (id === data.NullImage.id) {
+        return data.NullImage;
+    }
+    for (let image of $game.images) {
+        if (image.id === id) {
+            return image;
+        }
+    }
+    return null;
+}
+
 class BattlePlayerWindow {
     constructor(index) {
         this.index_ = index;
         // TODO: Centering?
-        this.window_ = new Window(80 * this.index_, 160, 80, 80);
+        this.window_ = new Window(80 * this.index_, 176, 80, 64);
     }
 
     update() {
@@ -23,6 +35,17 @@ class BattlePlayerWindow {
         this.drawShadowTextAt(context, actor.name, this.window_.x + 8, this.window_.y + 8, 255, 255, 255);
         this.drawShadowTextAt(context, "9999", this.window_.x + 8, this.window_.y + 8 + 16, 255, 255, 255);
         this.drawShadowTextAt(context, "999", this.window_.x + 8, this.window_.y + 8 + 32, 255, 255, 255);
+        let actorImage = imageById(actor.image);
+        let actorImg = new Image();
+        actorImg.src = actorImage.data;
+        let sx = 24;
+        let sy = 64;
+        let actorWidth = 24;
+        let actorHeight = 32;
+        let dx = this.window_.x + (this.window_.width - actorWidth) / 2;
+        let dy = this.window_.y - actorHeight + 8;
+        context.drawImage(actorImg, sx, sy, actorWidth, actorHeight, dx, dy, actorWidth, actorHeight);
+
         context.restore();
     }
 }
@@ -53,6 +76,17 @@ class BattleScene {
         }
         //this.window_.draw(context);
         //util.drawBitmapTextAt(context, "敵が出現!", this.window_.x + 8, this.window_.y + 8);
+
+        // TODO: Make a troop
+        {
+            let enemy = $game.enemies[0];
+            let enemyImage = imageById(enemy.image);
+            let enemyImg = new Image();
+            enemyImg.src = enemyImage.data;
+            let dx = (320 - enemyImg.width) / 2;
+            let dy = (160 - enemyImg.height) / 2;
+            context.drawImage(enemyImg, dx, dy, enemyImg.width, enemyImg.height);
+        }
 
         context.restore();
     }
