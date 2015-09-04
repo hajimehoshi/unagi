1f1c94a090a292ecaf4f4463df4e9363209bcec7
Hajime Hoshi <hajimehoshi@gmail.com>
2015-09-04 11:38:27 +0900

    Moving cursor in a command window

--- editor/defaultscripts/commandwindow.ts
+++ editor/defaultscripts/commandwindow.ts
@@ -1,19 +1,26 @@
 namespace game {
     export class CommandWindow {
         private window_: Window;
+        private commands_: string[];
+        private currentCommand_: number = 0; // TODO: currentCommandIndex_?
 
-        constructor(items: string[], x: number, y: number) {
+        constructor(commands: string[], x: number, y: number) {
             let width = 0;
             let height = 0;
-            for (let item of items) {
-                let size = $regularFont.calculateTextSize(item);
+            for (let command of commands) {
+                let size = $regularFont.calculateTextSize(command);
                 width = Math.max(size.width, width);
                 height += size.height;
             }
             width += Window.PADDING_X * 2;
             height += Window.PADDING_Y * 2;
             this.window_ = new Window(x, y, width, height);
-            this.window_.content = items.join('\n');
+            this.window_.content = commands.join('\n');
+
+            this.commands_ = [];
+            for (let command of commands) {
+                this.commands_.push(command);
+            }
         }
 
         public get x(): number { return this.window_.x; }
@@ -25,7 +32,17 @@ namespace game {
 
         public update() {
             this.window_.update();
-            this.window_.setSelection(Window.PADDING_X / 2, Window.PADDING_Y, this.window_.width - Window.PADDING_X, 16);
+
+            if ($input.isTrigger(Key.DOWN)) {
+                this.currentCommand_ = Math.min(this.currentCommand_ + 1, this.commands_.length - 1);
+            }
+            if ($input.isTrigger(Key.UP)) {
+                this.currentCommand_ = Math.max(this.currentCommand_ - 1, 0);
+            }
+
+            let x = Window.PADDING_X / 2;
+            let y = Window.PADDING_Y + 16 * this.currentCommand_;
+            this.window_.setSelection(x, y, this.window_.width - Window.PADDING_X, 16);
         }
 
         public draw(screen: graphics.Image) {
