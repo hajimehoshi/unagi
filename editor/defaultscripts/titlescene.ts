c7ca0570059338546209a4d58ea1a0f576f9999f
Hajime Hoshi <hajimehoshi@gmail.com>
2015-09-10 00:06:53 +0900

    Open/close the menu window at the title scene

--- editor/defaultscripts/titlescene.ts
+++ editor/defaultscripts/titlescene.ts
@@ -12,6 +12,7 @@ namespace game {
             this.menuWindow_.x = (320 - this.menuWindow_.width) / 2;
             this.menuWindow_.y = (240 - this.menuWindow_.height) - data.GRID_SIZE;
             this.menuWindow_.setEnabled(1, false);
+            this.menuWindow_.open();
         }
 
         public dispose() {
@@ -20,14 +21,20 @@ namespace game {
 
         public update() {
             this.menuWindow_.update();
-            if ($input.isTrigger(Key.ENTER)) {
-                if (this.menuWindow_.currentSelectionIndex === 1) {
-                    return;
-                }
+            if (this.menuWindow_.isAnimating) {
+                return;
+            }
+            if (this.menuWindow_.isClosed) {
                 let initialPosition = $gameData.playerInitialPosition;
                 let map = <data.Map>$idToData[initialPosition.mapId];
                 $sceneStack.push(new MapScene(map));
                 $gameState.moveTo(initialPosition.x, initialPosition.y);
+            }
+            if ($input.isTrigger(Key.ENTER)) {
+                if (this.menuWindow_.currentSelectionIndex === 1) {
+                    return;
+                }
+                this.menuWindow_.close();
             }
         }
 
